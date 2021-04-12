360bd5140bc7e9371e26dabd7827d9db4baf7e44
DanielIzaMtz <81535833+DanielIzaMtz@users.noreply.github.com>
2021-04-11 22:11:04 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebaseConfig = {
+    apiKey: "AIzaSyBkPph67XJTyNjNfYmnfTEhtuce-LemNnk",
+    authDomain: "autentifica42di.firebaseapp.com",
+    projectId: "autentifica42di",
+    storageBucket: "autentifica42di.appspot.com",
+    messagingSenderId: "1075643225364",
+    appId: "1:1075643225364:web:78d683d653e403308c9e01",
+    measurementId: "G-ZXPWN6K3M2"
+  };
